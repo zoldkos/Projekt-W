0a06dc214b205934fdbaf604d8bdbd3f18ecb4df
Zöld Ákos <92633224+zoldkos@users.noreply.github.com>
2024-04-12 12:31:27 +0200

    Add files via upload

---
+++ 11.fazis_hosting/regisztracio.php
@@ -0,0 +1,59 @@
+﻿<?php
+    if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
+        try {
+            // Kapcsolódás
+            $dbh = new PDO('mysql:host=mysql.rackhost.hu;dbname=c48546kapcsolat_urlap', 'c48546akos', 'adatbazispassword',
+                            array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+            $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+            
+            // Létezik-e már a felhasználói név?
+            $sqlSelect = "select id from felhasznalok where bejelentkezes = :bejelentkezes";
+            $sth = $dbh->prepare($sqlSelect);
+            $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo']));
+            if($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+                $uzenet = "A felhasználói név már foglalt!";
+                $ujra = "true";
+            }
+            else {
+                // Ha nem létezik, akkor regisztráljuk
+                $sqlInsert = "insert into felhasznalok(id, csaladi_nev, utonev, bejelentkezes, jelszo)
+                              values(0, :csaladinev, :utonev, :bejelentkezes, :jelszo)";
+                $stmt = $dbh->prepare($sqlInsert); 
+                $stmt->execute(array(':csaladinev' => $_POST['vezeteknev'], ':utonev' => $_POST['utonev'],
+                                     ':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => password_hash($_POST['jelszo'], PASSWORD_DEFAULT))); 
+                if($count = $stmt->rowCount()) {
+                    $newid = $dbh->lastInsertId();
+                    $uzenet = "A regisztrációja sikeres.<br>Azonosítója: {$newid}";                     
+                    $ujra = false;
+                }
+                else {
+                    $uzenet = "A regisztráció nem sikerült.";
+                    $ujra = true;
+                }
+            }
+        }
+        catch (PDOException $e) {
+            echo "Hiba: ".$e->getMessage();
+        }      
+    }
+    else {
+        header("Location: bejelentkezes.html");
+    }
+
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Regisztráció</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php if(isset($uzenet)) { ?>
+            <h1><?= $uzenet ?></h1>
+            <?php if($ujra) { ?>
+                <a href="bejelentkezes.html">Próbálja újra!</a>
+            <?php } ?>
+        <?php } ?>
+    </body>  
+</html>
